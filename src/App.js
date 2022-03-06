eb18e219738992eb962c370283917253766fc984
Sander Wegdam <sanderwegdam@gmail.com>
2022-03-06 13:57:18 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/menu';
+import { PLACES } from './shared/places';
+import Greet from './components/greet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {					
+  
+	constructor(props){
+		super(props);
+		
+		this.state = {
+			places: PLACES
+		};
+	}
+	
+	render(){
+		return (
+			<div className='App'>
+				<Navbar color="primary">
+					<div className="container" id="brand">
+						<NavbarBrand>NavBar</NavbarBrand>
+					</div>
+				</Navbar>
+				<Greet />
+				<Menu places={this.state.places} />
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import HelloWorld from './components/HelloWorld';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>    
-    <HelloWorld />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
