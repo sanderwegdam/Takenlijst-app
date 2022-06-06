ccfbd5a50a2e7811110192e0b3a0fdc47bc49892
Sander Wegdam <sanderwegdam@gmail.com>
2022-06-06 15:50:47 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { BrowserRouter as Router } from "react-router-dom";
 import TasksContainer from "./components/TasksContainer"
 import "./App.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+
       <TasksContainer />
-    </Router>  
+
   </React.StrictMode>, 
   document.getElementById("root")
 );
